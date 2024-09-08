504cb8c1900c23bb04f0f65064a5abdba42bd312
Zain Malik <xaineil121@gmail.com>
2024-09-08 13:50:35 +0500

    Initial Commit

---
+++ Source Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Source Control
+//
+//  Created by Zain Malik on 08/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Source Control/Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_ControlApp.swift
+//  Source Control
+//
+//  Created by Zain Malik on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
